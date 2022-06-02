f7eb1e505589c0fc00a26cd666db50ceb8455452
ung <test@abc.com>
2022-06-02 20:29:51 +0700

    finish 2June

--- lib/states/add_photo_multi.dart
+++ lib/states/add_photo_multi.dart
@@ -1,5 +1,12 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:admanyout/widgets/show_icon_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:wechat_assets_picker/wechat_assets_picker.dart';
+import 'package:image/image.dart' as Im;
 
 class AddPhotoMulti extends StatefulWidget {
   const AddPhotoMulti({Key? key}) : super(key: key);
@@ -10,10 +17,14 @@ class AddPhotoMulti extends StatefulWidget {
 
 class _AddPhotoMultiState extends State<AddPhotoMulti> {
   List<AssetEntity> assetEntitys = [];
+  var user = FirebaseAuth.instance.currentUser;
 
   Future<void> selectMultiImages() async {
     var result = await AssetPicker.pickAssets(context,
-        pickerConfig: const AssetPickerConfig(maxAssets: 20));
+        pickerConfig: AssetPickerConfig(
+          maxAssets: 20,
+        ));
+
     if (result!.isNotEmpty) {
       assetEntitys.addAll(result);
     }
@@ -31,6 +42,19 @@ class _AddPhotoMultiState extends State<AddPhotoMulti> {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
+        actions: [
+          ShowIconButton(
+              iconData: Icons.add_box_outlined,
+              pressFunc: () {
+                selectMultiImages();
+              }),
+          ShowIconButton(
+            iconData: Icons.arrow_forward_outlined,
+            pressFunc: () {
+              processUploadImage();
+            },
+          ),
+        ],
         foregroundColor: Colors.white,
         backgroundColor: Colors.black,
       ),
@@ -51,5 +75,26 @@ class _AddPhotoMultiState extends State<AddPhotoMulti> {
               ),
             ),
     );
+  }
+
+  Future<void> processUploadImage() async {
+    var files = <File>[];
+    for (var element in assetEntitys) {
+      final File? file = await element.file;
+
+      // Im.Image? image = Im.decodeImage(file!.readAsBytesSync());
+      // Im.Image smallImage = Im.copyResize(image!, width: 800, height: 800);
+
+      String nameFile = '${user!.uid}${Random().nextInt(10000000)}.jpg';
+      FirebaseStorage storage = FirebaseStorage.instance;
+      Reference reference = storage.ref().child('/test/$nameFile');
+      UploadTask uploadTask = reference.putFile(file!);
+      await uploadTask.whenComplete(() async {
+        await reference.getDownloadURL().then((value) {
+          String urlImage = value;
+          print('urlImage ==>> $urlImage');
+        });
+      });
+    }
   }
 }
