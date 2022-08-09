d3c532905fc580ad548d6cacd6c8fc91f46201a3
ung <test@abc.com>
2022-08-09 20:32:13 +0700

    finish 9aug

--- lib/states/add_room_meeting.dart
+++ lib/states/add_room_meeting.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
+import 'dart:math';
 
+import 'package:admanyout/models/room_model.dart';
 import 'package:admanyout/models/user_model.dart';
 import 'package:admanyout/utility/my_constant.dart';
 import 'package:admanyout/utility/my_firebase.dart';
@@ -13,9 +15,12 @@ import 'package:admanyout/widgets/show_image.dart';
 import 'package:admanyout/widgets/show_text.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:random_password_generator/random_password_generator.dart';
 
 class AddRoomMeeting extends StatefulWidget {
   const AddRoomMeeting({Key? key}) : super(key: key);
@@ -28,13 +33,38 @@ class _AddRoomMeetingState extends State<AddRoomMeeting> {
   var user = FirebaseAuth.instance.currentUser;
   UserModel? userModel;
   File? file;
-  String? linkMeeting, urlImageRoom, nameRoom, nameOwner, tokenOwner;
-  Timestamp? addTimeStamp;
+  String? linkMeeting, urlImageRoom, nameRoom, nameOwner, tokenOwner, idRoom;
+  String linkContact = '', password = '';
+  bool usePassword = false;
+
+  GlobalKey globalKey = GlobalKey();
 
   @override
   void initState() {
     super.initState();
     processFindUserModel();
+    readAllRoom();
+  }
+
+  void createPassword() {
+    final genPassword = RandomPasswordGenerator();
+    password = genPassword.randomPassword(
+        letters: true,
+        uppercase: true,
+        numbers: true,
+        specialChar: false,
+        passwordLength: 6);
+    setState(() {});
+    print('password ====> $password');
+  }
+
+  Future<void> readAllRoom() async {
+    await FirebaseFirestore.instance.collection('room').get().then((value) {
+      int amountRoom = value.docs.length;
+      amountRoom = amountRoom + 100 + 1;
+      idRoom = '#L$amountRoom';
+      print('idRoom ===> $idRoom');
+    });
   }
 
   Future<void> processFindUserModel() async {
@@ -50,64 +80,75 @@ class _AddRoomMeetingState extends State<AddRoomMeeting> {
       backgroundColor: MyStyle.bgColor,
       appBar: newAppBar(),
       body: LayoutBuilder(builder: (context, BoxConstraints boxConstraints) {
-        return ListView(
-          children: [
-            createCenter(
-              boxConstraints: boxConstraints,
-              widget: ShowForm(
-                colorTheme: MyStyle.dark,
-                label: 'อยากบอกอะไร',
-                iconData: Icons.room,
-                changeFunc: (p0) {
-                  nameRoom = p0.trim();
-                },
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Stack(
-                  children: [
-                    SizedBox(
-                      width: boxConstraints.maxWidth * 0.6,
-                      height: boxConstraints.maxWidth * 0.6,
-                      child: InkWell(
-                        onTap: () {
-                          processTakePhoto(source: ImageSource.gallery);
-                        },
-                        child: file == null
-                            ? const ShowImage(
-                                path: 'images/logo.png',
-                              )
-                            : Image.file(file!),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: ShowIconButton(
-                        color: Colors.red,
-                        size: 36,
-                        iconData: Icons.camera,
-                        pressFunc: () {
-                          processTakePhoto(source: ImageSource.camera);
-                        },
-                      ),
-                    )
-                  ],
+        return GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () => FocusScope.of(context).requestFocus(FocusScopeNode()),
+          child: ListView(
+            children: [
+              createCenter(
+                boxConstraints: boxConstraints,
+                widget: ShowForm(
+                  colorTheme: MyStyle.dark,
+                  label: 'อยากบอกอะไร',
+                  iconData: Icons.room,
+                  changeFunc: (p0) {
+                    nameRoom = p0.trim();
+                  },
                 ),
-              ],
-            ),
-            createCenter(
-              boxConstraints: boxConstraints,
-              widget: ShowFormLong(
-                label: 'กรุณาใส่ Link Meeting',
-                changeFunc: (p0) {
-                  linkMeeting = p0.trim();
-                },
               ),
-            ),
-            createCenter(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Stack(
+                    children: [
+                      SizedBox(
+                        width: boxConstraints.maxWidth * 0.6,
+                        height: boxConstraints.maxWidth * 0.6,
+                        child: InkWell(
+                          onTap: () {
+                            processTakePhoto(source: ImageSource.gallery);
+                          },
+                          child: file == null
+                              ? const ShowImage(
+                                  path: 'images/logo.png',
+                                )
+                              : Image.file(file!),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: ShowIconButton(
+                          color: Colors.red,
+                          size: 36,
+                          iconData: Icons.camera,
+                          pressFunc: () {
+                            processTakePhoto(source: ImageSource.camera);
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              createCenter(
+                boxConstraints: boxConstraints,
+                widget: ShowFormLong(
+                  label: 'กรุณาใส่ Link Meeting',
+                  changeFunc: (p0) {
+                    linkMeeting = p0.trim();
+                  },
+                ),
+              ),
+              createCenter(
+                  boxConstraints: boxConstraints,
+                  widget: ShowFormLong(
+                    label: 'Link ที่ใช้ติดต่อ',
+                    changeFunc: (p0) {
+                      linkContact = p0.trim();
+                    },
+                  )),
+              createCenter(
                 boxConstraints: boxConstraints,
                 widget: ShowButton(
                   label: 'OK',
@@ -116,10 +157,71 @@ class _AddRoomMeetingState extends State<AddRoomMeeting> {
                       Fluttertoast.showToast(msg: 'กรุณากรอก อยากบอกอะไร');
                     } else if (linkMeeting?.isEmpty ?? true) {
                       Fluttertoast.showToast(msg: 'กรุณากรอก Link Meeting');
+                    } else {
+                      if (file == null) {
+                        urlImageRoom = MyConstant.urlLogo;
+                        processInsertRoom();
+                      } else {
+                        processUploadImage();
+                      }
                     }
                   },
-                ))
-          ],
+                ),
+              ),
+              createCenter(
+                boxConstraints: boxConstraints,
+                widget: SwitchListTile(
+                  title: ShowText(
+                    label: 'เปิดเพื่อใช้ รหัสผ่าน',
+                    textStyle: MyConstant().h3BlackStyle(),
+                  ),
+                  value: usePassword,
+                  onChanged: (value) {
+                    usePassword = value;
+                    if (!usePassword) {
+                      password = '';
+                    }
+                    setState(() {});
+                  },
+                ),
+              ),
+              usePassword
+                  ? createCenter(
+                      boxConstraints: boxConstraints,
+                      widget: ShowButton(
+                        label: 'กดเพื่อสร้างรหัสผ่าน',
+                        pressFunc: () {
+                          createPassword();
+                        },
+                      ),
+                    )
+                  : const SizedBox(),
+              password.isNotEmpty
+                  ? createCenter(
+                      addWidth: 50,
+                      boxConstraints: boxConstraints,
+                      widget: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          ShowText(
+                            label: 'RoomPassword: ',
+                            textStyle: MyConstant().h2BlackBBBStyle(),
+                          ),
+                          ShowText(
+                            label: password,
+                            textStyle: MyConstant().h2redStyle(),
+                          ),
+                        ],
+                      ))
+                  : const SizedBox(),
+              password.isNotEmpty
+                  ? RepaintBoundary(
+                      key: globalKey,
+                      child: createCenter(boxConstraints: boxConstraints, widget: QrImage(data: password)),
+                    )
+                  : const SizedBox(),
+            ],
+          ),
         );
       }),
     );
@@ -149,12 +251,15 @@ class _AddRoomMeetingState extends State<AddRoomMeeting> {
   }
 
   Row createCenter(
-      {required BoxConstraints boxConstraints, required Widget widget}) {
+      {required BoxConstraints boxConstraints,
+      required Widget widget,
+      double? addWidth}) {
+    double width = addWidth ?? 0;
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SizedBox(
-          width: boxConstraints.maxWidth * 0.6,
+          width: boxConstraints.maxWidth * 0.6 + width,
           child: widget,
         ),
       ],
@@ -171,5 +276,34 @@ class _AddRoomMeetingState extends State<AddRoomMeeting> {
       file = File(result.path);
       setState(() {});
     }
+  }
+
+  Future<void> processInsertRoom() async {
+    DateTime dateTime = DateTime.now();
+
+    RoomModel roomModel = RoomModel(
+        idRoom: idRoom!,
+        linkContact: linkContact,
+        linkRoom: linkMeeting!,
+        nameRoom: nameRoom!,
+        password: password,
+        timeDateAdd: Timestamp.fromDate(dateTime),
+        uidOwner: user!.uid,
+        urlImage: urlImageRoom!,
+        usePassword: false);
+  }
+
+  Future<void> processUploadImage() async {
+    String nameImage = '${user!.uid}${Random().nextInt(10000)}.jpg';
+    FirebaseStorage storage = FirebaseStorage.instance;
+    Reference reference = storage.ref().child('room/$nameImage');
+    UploadTask task = reference.putFile(file!);
+    await task.whenComplete(() async {
+      await reference.getDownloadURL().then((value) {
+        urlImageRoom = value;
+        print('urlImageRoom ==> $urlImageRoom');
+        processInsertRoom();
+      });
+    });
   }
 }
